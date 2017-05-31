a910e2a643f76ed69d130bbd32c2d22dd3ac0460
JasonBaby <GeniusJason@gmail.com>
2017-05-31 09:55:08 +0800

    Add Basic Global Variables

---
+++ global/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H_INCLUDED
+#define CONFIG_H_INCLUDED
+
+
+typedef int millisecond;
+
+extern millisecond globalOffset;
+extern millisecond visualOffset;
+
+
+#endif // CONFIG_H_INCLUDED
