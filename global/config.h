791a490d04e2b3c039ffaa19a2273dcf82ddff84
JasonBaby <GeniusJason@gmail.com>
2017-05-31 15:01:06 +0800

    Add judgeWindows Documentation

--- global/config.h
+++ global/config.h
@@ -10,6 +10,7 @@ typedef long long Nanosecond;
 extern Millisecond globalOffset;
 extern Millisecond visualOffset;
 
+/** The judge windows.
 extern vector<Nanosecond> judgeWindows;
 
 #endif // CONFIG_H_INCLUDED
