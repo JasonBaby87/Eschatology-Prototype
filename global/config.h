f680601dc4c8a7ef73fb2eb2f879bd86bdd94152
JasonBaby <GeniusJason@gmail.com>
2017-06-02 10:42:22 +0800

    Change the Time Types to duration

--- global/config.h
+++ global/config.h
@@ -1,11 +1,14 @@
-#include <vector>
-
 #ifndef CONFIG_H_INCLUDED
 #define CONFIG_H_INCLUDED
 
+#include <vector>
+#include <chrono>
 
-typedef int Millisecond;
-typedef long long Nanosecond;
+using namespace std;
+
+typedef duration<int,milli> Millisecond;
+typedef duration<long long,micro> Microsecond;
+typedef duration<long long,nano> Nanosecond;
 
 extern Millisecond globalOffset;
 extern Millisecond visualOffset;
