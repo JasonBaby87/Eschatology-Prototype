5db42a8ef299f487933cbdadf9f94540d0f8ca45
JasonBaby <GeniusJason@gmail.com>
2017-06-02 12:13:08 +0800

    Fix type

--- gameplay/chartPlayer.h
+++ gameplay/chartPlayer.h
@@ -1,7 +1,7 @@
 #ifndef CHARTPLAYER_H_INCLUDED
 #define CHARTPLAYER_H_INCLUDED
 
-#include "../global/types.h"
+#include "../global/type.h"
 #include "../global/config.h"
 #include "../global/state.h"
 #include "../utilities/musicPlayer.h"
