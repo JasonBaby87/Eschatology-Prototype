af630e8cfcb19a696ac72e0db118e774c3864101
Timmatt <qpalzm19970904@gmail.com>
2017-06-02 22:16:40 +0800

    test 1.51
    用數字鍵1~4切換同一張圖片的clips

--- main.cpp
+++ main.cpp
@@ -9,18 +9,44 @@ const int SCREEN_HEIGHT=480;
 SDL_Window *window;
 SDL_Renderer *renderer;
 
-void ApplySurface(int x,int y,SDL_Texture *tex)
+//裁切圖片的每一小塊圖片都是一樣長寬而且是有序排列的
+SDL_Rect* clipRect(int iW,int iH,int row,int column) //要裁切的每一份小圖的長寬和小圖們總共幾行幾列
+{
+    SDL_Rect* clips=new SDL_Rect[row*column];
+    for (int i=0,c=0;i<row*column;i++)
+    {
+        if (i!=0 && i%row== 0)
+            c++;
+        clips[i].x = c * iW;
+        clips[i].y = i % row * iH;
+        clips[i].w = iW;
+        clips[i].h = iH;
+    }
+    return clips;
+}
+
+void ApplySurface(int x,int y,SDL_Texture *tex,SDL_Rect *clip=NULL)
 {
     SDL_Rect pos; //一個長方形
-    pos.x=x;
-    pos.y=y;
-    SDL_QueryTexture(tex,NULL,NULL,&pos.w,&pos.h); //把texture的長寬傳出給pos，我們要1:1畫圖
 
-    SDL_RenderCopy(renderer,tex,NULL,&pos);
+    if (clip!=NULL) //如果有需要剪裁
+    {
+        pos.w = clip->w;
+        pos.h = clip->h;
+    }
+    else
+        SDL_QueryTexture(tex,NULL,NULL,&pos.w,&pos.h); //把texture的長寬傳出給pos，我們要1:1畫圖
+
+    pos.x=x-pos.w/2; //以物體的中心點做定位錨
+    pos.y=y-pos.h/2;
+
+    SDL_RenderCopy(renderer,tex,clip,&pos);
     //把texture畫上去
-    //第三個參數是指向源矩形的指針，也就是從圖像上裁剪下的一塊矩形，這裡代表繪製整個源圖像
-    //第四個參數是指向目標矩形的指針，這裡代表把它畫在屏幕上(0,0)的位置，並拉伸這個圖像讓它填滿整個窗口
+    //第三個參數是指向源矩形的指針，也就是從圖像上裁剪下的一塊矩形，若是NULL代表繪製整個源圖像
+    //第四個參數是指向目標矩形的指針，若是NULL代表把它畫在屏幕上(0,0)的位置，並拉伸這個圖像讓它填滿整個窗口
 }
+
+
 
 int main(int argc,char* args[])
 {
@@ -32,7 +58,10 @@ int main(int argc,char* args[])
     // SDL_RENDERER_PRESENTVSYNC，使用SDL_RendererPresent這個函數，他會以顯示器的刷新率來更新畫面
 
     SDL_Texture* bg=IMG_LoadTexture(renderer,"img/bg.jpg");
-    SDL_Texture* yellowDot=IMG_LoadTexture(renderer,"img/aluren.png");
+    SDL_Texture* buttons=IMG_LoadTexture(renderer,"img/buttons.png");
+
+    SDL_Rect* clips=clipRect(245,245,2,2);
+    int useClip=0;
 
     SDL_Event e;
     bool quit = false;
@@ -40,13 +69,34 @@ int main(int argc,char* args[])
     {
         while (SDL_PollEvent(&e))
         {
-    		if (e.type == SDL_QUIT)
+
+            if (e.type == SDL_QUIT) //單擊右上角的X
             {
     			quit = true;
     		}
     		if (e.type == SDL_KEYDOWN)
             {
-    			quit = true;
+                switch (e.key.keysym.sym)
+                {
+                    case SDLK_1:
+                        useClip = 0;
+                        break;
+                    case SDLK_2:
+                        useClip = 1;
+                        break;
+                    case SDLK_3:
+                        useClip = 2;
+                        break;
+                    case SDLK_4:
+                        useClip = 3;
+                        break;
+                    //For quitting, escape key
+                    case SDLK_ESCAPE:
+                        quit = true;
+                        break;
+                    default:
+                        break;
+                }
     		}
     		if (e.type == SDL_MOUSEBUTTONDOWN)
             {
@@ -55,18 +105,15 @@ int main(int argc,char* args[])
 
             SDL_RenderClear(renderer); //清屏
 
-            ApplySurface(0,0,bg);
-            int iW, iH;
-            SDL_QueryTexture(yellowDot, NULL, NULL, &iW, &iH);
-            ApplySurface(SCREEN_WIDTH/2-iW/2,SCREEN_HEIGHT/2-iH/2,yellowDot);
-            //因為SDL會pin在圖片的左上角，所以要讓圖片放在正中央需要計算一下
+            ApplySurface(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,bg);
+            ApplySurface(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,buttons,&clips[useClip]);
 
             SDL_RenderPresent(renderer); //更新螢幕畫面
         }
     }
 
     SDL_DestroyTexture(bg);
-    SDL_DestroyTexture(yellowDot);
+    SDL_DestroyTexture(buttons);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 
