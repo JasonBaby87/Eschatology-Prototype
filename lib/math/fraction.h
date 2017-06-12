2429b2018ced89ef4f2712ea1a5329e36e0b0b0c
JasonBaby <GeniusJason@gmail.com>
2017-06-12 12:34:51 +0800

    Fraction Header Mostly Done
    The operator overloading will be added when needed.

--- lib/math/fraction.h
+++ lib/math/fraction.h
@@ -1,6 +1,35 @@
 #ifndef FRACTION_H_INCLUDED
 #define FRACTION_H_INCLUDED
 
+template <typename N>
+class ImproperFraction
+{
+private:
+    N numerator;
+    N denominator;
+protected:
+    ImproperFraction& reduce();
+public:
+    ImproperFraction(N = 0, N = 1);
+	N getInteger() const;
+	N getNumerator() const;
+	N getDenominator() const;
+	// Operator overloading when needed
+};
 
+template <typename N1, N2>
+class Fraction
+{
+private:
+	N1 integer;
+	ImproperFraction<N2> fraction;
+	Fraction& reduce();
+public:
+	Fraction(N1 = 0, N2 = 0, N2 = 1);
+	N1 getInteger() const;
+	N2 getNumerator() const;
+	N2 getDenominator() const;
+	// Operator overloading when needed
+};
 
 #endif // FRACTION_H_INCLUDED
