a755daf4fe7956f631d65f74d9fd2be97d521e37
JasonBaby <GeniusJason@gmail.com>
2017-06-12 21:42:34 +0800

    Fill in Some Implementations
    Completed:
    1. ChartPlayer constructor
    
    Started:
    1. ChartPlayer::getNotePositions

--- lib/gameplay/chartPlayer.cpp
+++ lib/gameplay/chartPlayer.cpp
@@ -2,23 +2,49 @@
 #define CHARTPLAYER_CPP_INCLUDED
 
 #include <istream>
+#include <list>
 #include <vector>
 #include "chartPlayer.h"
 #include "../global/exception.h"
 
 Note::Note(Beat b): beat(b)
 {
-	alive = false;
+
 }
 
-ChartPlayer::ChartPlayer(MusicPlayer&, istream& data)
+void ChartPlayer::registerMisses()
 {
 	throw NotImplementedException();
+}
+
+ChartPlayer::ChartPlayer(MusicPlayer& music, istream& data): music(music)
+{
+	data >> songOffset >> bpm;
+    Beat beat;
+    while (data >> beat)
+	{
+		Note* note = new Note(beat);
+        notes.push_back(note);
+	}
+
+    sort(notes.begin(), notes.end(), earlierThan);
 }
 
 vector<pair<BeatDuration,Note*>>& ChartPlayer::getNotePositions(BeatDuration)
 {
-	throw NotImplementedException();
+	registerMisses();
+
+	vector<pair<BeatDuration,Note*>> result;
+
+	for (auto it = notes.begin(); it != notes.end(); it++)
+	{
+		// TODO more thing D: need to calculate how far away each notes are
+		result.push_back(*it);
+		// TODO check stuff
+		throw NotImplementedException("Beat Duration not checked.");
+	}
+
+	return result;
 }
 
 void ChartPlayer::hit()
