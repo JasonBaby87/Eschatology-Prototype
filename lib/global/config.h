46c58df9980ea85b9742735e617f9cfbe9b35489
JasonBaby <GeniusJason@gmail.com>
2017-06-17 20:37:23 +0800

    Fix config.h
    Now this actually fixes it D:

--- lib/global/config.h
+++ lib/global/config.h
@@ -7,12 +7,12 @@
 using namespace std;
 
 /** Shifts all the notes later */
-extern Milliseconds globalOffset();
+extern Milliseconds globalOffset = Milliseconds();
 /** Shifts all the visual later */
-extern Milliseconds visualOffset();
+extern Milliseconds visualOffset = Milliseconds();
 
 /** The judge windows.
 	<p>The smaller the indices the stricter it is. */
-extern vector<Nanoseconds> judgeWindows();
+extern vector<Nanoseconds> judgeWindows = vector<Nanoseconds>();
 
 #endif // CONFIG_H_INCLUDED
