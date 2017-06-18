9ccec8820693f361e520102ad2fe66b5f533e0d6
JasonBaby <GeniusJason@gmail.com>
2017-06-18 20:57:45 +0800

    Start config.cpp
    Gotta change something in another branch

--- lib/global/config.cpp
+++ lib/global/config.cpp
@@ -1,1 +1,15 @@
+#ifndef CONFIG_CPP_INCLUDED
+#define CONFIG_CPP_INCLUDED
 
+#include <istream>
+#include "type.h"
+#include "config.h"
+
+
+void loadConfigurations(istream& is)
+{
+    Time
+}
+
+
+#endif // CONFIG_CPP_INCLUDED
