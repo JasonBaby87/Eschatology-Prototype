7249a68a467863e0db761d34199a52e02c88dafd
JasonBaby <GeniusJason@gmail.com>
2017-06-18 20:22:09 +0800

    Fix registerMisses
    Overflowing double sure is fun, eh?

--- lib/gameplay/chartPlayer.cpp
+++ lib/gameplay/chartPlayer.cpp
@@ -25,10 +25,10 @@ void ChartPlayer::registerMisses()
 	while(!notes.empty())
 	{
 		Time currentTime = chrono::duration_cast<chrono::nanoseconds>
-			(chrono::high_resolution_clock::now() - startTime - songOffset - globalOffset).count() * 1e9;
+			(chrono::high_resolution_clock::now() - startTime - songOffset - globalOffset).count() / 1e9;
 
 		Note* note = notes.front();
-		Time noteTime = static_cast<Time>(note->beat) / bpm * 6e10;
+		Time noteTime = static_cast<Time>(note->beat) / bpm * 60;
 		Time timeDifference = noteTime - currentTime;
 
 		if (timeDifference < chrono::duration_cast<chrono::duration<Time>>(-judgeWindows.back()).count())
