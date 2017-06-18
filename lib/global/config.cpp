00abd51431b6eb924ac870599bf873302e5ae4bf
JasonBaby <GeniusJason@gmail.com>
2017-06-18 21:10:29 +0800

    Finish loadConfigurations
    :)

--- lib/global/config.cpp
+++ lib/global/config.cpp
@@ -2,14 +2,34 @@
 #define CONFIG_CPP_INCLUDED
 
 #include <istream>
+#include <chrono>
+#include <cmath>
 #include "type.h"
 #include "config.h"
 
+using namespace std;
 
 void loadConfigurations(istream& is)
 {
-    Time
-}
+    Time doubleTime;
 
+	is >> doubleTime;
+	globalOffset = Milliseconds(lround(doubleTime * 1e3));
+
+	is >> doubleTime;
+	visualOffset = Milliseconds(lround(doubleTime * 1e3));
+
+	judgeWindows.clear();
+	Judgement numOfJudgements;
+	Nanoseconds judgeWindow;
+
+	is >> numOfJudgements;
+	for (Judgement i = 0; i < numOfJudgements; i++)
+	{
+		is >> doubleTime;
+		judgeWindow = Nanoseconds(lround(doubleTime * 1e9));
+        judgeWindows.push_back(judgeWindow);
+	}
+}
 
 #endif // CONFIG_CPP_INCLUDED
